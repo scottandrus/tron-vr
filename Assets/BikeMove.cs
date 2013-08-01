a83d5fb1c619b92dc79020f4e3a8ba946d504714
Graham Gaylor <z.graham.gaylor@gree.net>
2013-07-31 22:41:29 -0700

    First commit.

---
+++ Assets/BikeMove.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BikeMove : MonoBehaviour {
+	public Transform cubePrefab;
+    public float speed = 10.0F;
+    public float rotationSpeed = 100.0F;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+		if(translation > 0) {
+			Instantiate(cubePrefab, transform.position, transform.rotation);
+		}
+	}
+}
